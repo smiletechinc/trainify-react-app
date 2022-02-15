4e5de647a0860219ab8ddd6bc931de858a77af66
hfshan247 <hfshan247@gmail.com>
2022-02-15 21:27:42 +0500

    Setup for timer.

--- App3.tsx
+++ App3.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { StyleSheet, Text, View, Dimensions, Platform } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Platform, Alert } from "react-native";
 
 import { Camera } from "expo-camera";
 
@@ -65,6 +65,8 @@ export default function UsamaCameraContainer() {
   const [cameraType, setCameraType] = useState<CameraType>(
     Camera.Constants.Type.back
   );
+
+  const [canAdd, setCanAdd] = useState(true);
 
   // For Serve Grading
   let AGradeServe = 0;
@@ -195,9 +197,16 @@ export default function UsamaCameraContainer() {
 
         let totalServesTemp = totalServes + 1;
         setTotalServes(totalServesTemp);
-        increment();
+        // Alert.alert(JSON.stringify(canAdd));
+        // Alert.alert(JSON.stringify(timerSeconds));
+        if (canAdd === true) {
+          Alert.alert("YES");
+          increment();
+          setCanAdd(false);
+          clearTimer(getDeadTime());
+        }
         console.log(totalServesTemp);
-      } else if (skipFrameCount > 0 && skipFrameCount < 30) {
+      } else if (skipFrameCount > 0 && skipFrameCount < 80) {
         skipFrameCount = skipFrameCount + 1;
       } else {
         skipFrameCount = 0;
@@ -401,8 +410,11 @@ export default function UsamaCameraContainer() {
   const renderFps = () => {
     return (
       <View style={styles.fpsContainer}>
-        <Text>fps: {fps}</Text>
+        <Text>FPS: {fps}</Text>
         <Text>Total {count}</Text>
+        <Text>Timer {timer}</Text>
+        <Text>Seconds {timerSeconds}</Text>
+        <Text>CanAdd { JSON.stringify(canAdd)}</Text>
       </View>
     );
   };
@@ -476,6 +488,101 @@ export default function UsamaCameraContainer() {
         return 0;
     }
   };
+
+
+/* Timer start*/
+const Ref = useRef(null);
+
+const [timer, setTimer] = useState('10000');
+const [timerSeconds, setSeconds] = useState(0);
+
+const getTimeRemaining = (e) => {
+  const total = Date.parse(e) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / 1000 * 60 * 60) % 24);
+  return {
+      total, hours, minutes, seconds
+  };
+}
+
+
+const startTimer = (e) => {
+  let { total, hours, minutes, seconds } 
+              = getTimeRemaining(e);
+  if (total >= 0) {
+      // update the timer
+      // check if less than 10 then we need to 
+      // add '0' at the begining of the variable
+      // setTimer(
+      //     (hours > 9 ? hours : '0' + hours) + ':' +
+      //     (minutes > 9 ? minutes : '0' + minutes) + ':'
+      //     + (seconds > 9 ? seconds : '0' + seconds)
+      // )
+      // setTimer(JSON.stringify(total));
+      setSeconds(seconds);
+  }
+}
+
+
+const clearTimer = (e) => {
+  // If you adjust it you should also need to
+  // adjust the Endtime formula we are about
+  // to code next    
+  // setTimer('10000');
+  setSeconds(10);
+
+  // If you try to remove this line the 
+  // updating of timer Variable will be
+  // after 1000ms or 1sec
+  if (Ref.current) clearInterval(Ref.current);
+  const id = setInterval(() => {
+      startTimer(e);
+  }, 1000)
+  Ref.current = id;
+}
+
+const getDeadTime = () => {
+  let deadline = new Date();
+
+  // This is where you need to adjust if 
+  // you entend to add more time
+  deadline.setSeconds(deadline.getSeconds() + 10);
+  return deadline;
+}
+
+// We can use useEffect so that when the component
+// mount the timer will start as soon as possible
+
+// We put empty array to act as componentDid
+// mount only
+useEffect(() => {
+  clearTimer(getDeadTime());
+}, []);
+
+// Another way to call the clearTimer() to start
+// the countdown is via action event from the
+// button first we create function to be called
+// by the button 
+const onClickReset = () => {
+  clearTimer(getDeadTime());
+}
+/* Timer end */
+
+useEffect(()=> {
+  if (timerSeconds >= 2){
+    setCanAdd(false);
+  } else if (timerSeconds < 2) {
+    setCanAdd(true)
+    // Alert.alert('Can add.')
+  }
+},[timerSeconds])
+
+useEffect(() => {
+  if (tfReady) {
+    clearTimer(getDeadTime());
+  }
+}, [tfReady]);
 
   if (!tfReady) {
     return (
