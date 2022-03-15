da50d0b8d8533d7c2db47a6d1e873fb54a4eddf0
Farhan-96 <farhan96fareed@gmail.com>
2022-03-15 16:03:43 +0500

    Grid IPHone doen

--- src/components/grid/grid_style.tsx
+++ src/components/grid/grid_style.tsx
@@ -1,32 +1,33 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   gridView: {
     marginTop: 10,
     flex: 1,
-  },  
+  },
   itemContainer: {
-  borderStyle: "solid",
-  justifyContent: 'flex-end',
-  borderRadius: 8,
-  marginLeft: 32,
-  marginTop: 8,
-  padding: 10,
-  height: 300,
-  width: 210
+    borderStyle: 'solid',
+    justifyContent: 'flex-end',
+    borderRadius: 8,
+    marginTop: 10,
+    padding: 10,
+    height: 300,
+    backgroundColor: '#D3D3D3',
+    borderWidth: 2,
+    borderColor: 'grey',
+    marginLeft: 10,
   },
   itemName: {
-    width:80,
-  fontSize: 16,
-  color: '#0096FF',
-  fontWeight: '600',
+    fontSize: 16,
+    color: '#0096FF',
+    fontWeight: '600',
+    textAlign: 'center',
   },
   itemCode: {
-    width:80,
     fontWeight: '600',
     fontSize: 12,
     color: 'black',
-    textAlign:"right"
-  }
+    textAlign: 'center',
+  },
 });
-export default styles  ;
+export default styles;
