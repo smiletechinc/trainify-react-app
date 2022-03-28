e6993415c25cfd70300659258180de959cf4f9df
Syed Salman Haider Kazmi <14bscsskazmi@seecs.edu.pk>
2022-03-28 18:19:05 +0200

    Update useMediaUpload.tsx

--- src/hooks/useMediaUpload.tsx
+++ src/hooks/useMediaUpload.tsx
@@ -71,44 +71,28 @@ export const useMediaUpload = props => {
     const fileURI = imageData.uri;
     const fileType = imageData.type;
 
-    const storageRef = ref(storage, `images/${fileName}`);
+    const videoReference = storage1().ref(`images/${fileName}`);
+
     const fileImage = JSON.parse(
       JSON.stringify({ uri: fileURI, type: fileType, name: fileName }),
     );
 
     try {
-      const img = await fetch(fileImage.uri);
-      if (img) {
-        const bytes = await img.blob();
-        if (bytes) {
-          console.log('ready file: ', fileImage);
+      const task = videoReference.putFile(fileImage.uri);
 
-          uploadBytes(storageRef, bytes)
-            .then(response => {
-              console.log('Thumbnail uploaded: ', response);
-              // onSuccess(response);
-              // let fileName = response.metadata.name
-              let filePath = response.ref._location.path_;
-              (() => {
-                getThumbnailURL(filePath);
-              })();
-            })
-            .catch(error => {
-              console.error(error);
-              setUploadThumbnailFailure(true);
-              setUploading(false);
-              setUploadThumbnailFailure(true);
-            });
-        } else {
-          Alert.alert('Error uploading humbnail 2');
-          setUploading(false);
-          setCurrentStatus('Error uploading thumbnail');
-        }
-      } else {
-        Alert.alert('Error uploading humbnail 1');
+      task.then((response) => {
+        console.log('Thumbnail uploaded: ', response);
+        let filePath = response.metadata.fullPath;
+        (() => {
+          getThumbnailURL(filePath);
+        })();
+
+      }).catch((error) => {
+        console.error(error);
+        setUploadThumbnailFailure(true);
         setUploading(false);
-        setCurrentStatus('Error uploading thumbnail');
-      }
+        setUploadThumbnailFailure(true);
+      });
     } catch (error) {
       Alert.alert('Error uploading humbnail', JSON.stringify(error));
       setUploading(false);
@@ -149,6 +133,10 @@ export const useMediaUpload = props => {
     const videoReference = storage1().ref(`videos/${fileName}`);
     try {
       const task = videoReference.putFile(fileURI);
+      task.on('state_changed', taskSnapshot => {
+        const percentage = Math.round((taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 100);
+        setCurrentStatus(`uploading video ${percentage}%`);
+      });
       task.then((response) => {
         console.log('File uploaded successfully:.... ', response);
         let filePath = response.metadata.fullPath;
@@ -168,53 +156,6 @@ export const useMediaUpload = props => {
       setUploading(false);
       setCurrentStatus('Error uploading video');
     }
-
-    // try {
-    //   const img = await fetch(fileImage.uri);
-    //   if (img) {
-    //     // Alert.alert('Bytes for Videos');
-    //     console.log('image for video: ', img);
-    //     const bytes = await img.blob();
-    //     if (bytes) {
-    //       console.log('bytes for video: ', fileImage);
-    //       try {
-    //         uploadBytes(storageRef, bytes)
-    //           .then(response => {
-    //             console.log('Video uploaded: ', response);
-    //             // onSuccess(response);
-    //             // let fileName = response.metadata.name
-    //             let filePath = response.ref._location.path_;
-    //             (() => {
-    //               getVideoURL(filePath);
-    //             })();
-    //           })
-    //           .catch(error => {
-    //             console.error(error);
-    //             setUploadVideoFailure(true);
-    //             setUploading(false);
-    //           });
-    //       } catch (error) {
-    //         Alert.alert('Error uploading video, ');
-    //         setUploading(false);
-    //         setCurrentStatus('Error uploading video');
-    //       }
-    //     } else {
-    //       Alert.alert('Error uploading video 2, ');
-    //       setUploading(false);
-    //       setCurrentStatus('Error uploading video 2');
-    //     }
-    //   } else {
-    //     Alert.alert('Error uploading video 1, ');
-    //     setUploading(false);
-    //     setCurrentStatus('Error uploading video 1');
-    //   }
-    // } catch (error) {
-    //   Alert.alert('Error uploading video 0, ');
-    //   setUploading(false);
-    //   setCurrentStatus('Error uploading video 0');
-    // }
-
-
   }, []);
 
   const getVideoURL = (fileName: string) => {
